78aef144259392097e3b4080434c5593b55a488f
Matias gomez <mrmatiasgomez@gmail.com>
2023-07-20 15:03:12 +0200

    Ajout du front et de l'appel des badges

--- components/Badge.js
+++ components/Badge.js
@@ -1,101 +1,98 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import {
   Image,
+  Modal,
+  Pressable,
   SafeAreaView,
   ScrollView,
   StyleSheet,
   Text,
   View,
 } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Badge() {
+  const [badges, setBadges] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [badgeSelected, setBadgeSelected] = useState(-1);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get("http://10.74.0.59:4000/api/badges");
+      console.log(response.data);
+      setBadges(response.data);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, marginTop: 50 }}>
       <ScrollView>
         <View style={styles.container}>
           <View style={styles.row}>
-            <View style={styles.badge}>
-              {/* Badge 1 */}
-              <Image
-                style={styles.imageBadge}
-                source={{
-                  uri: "https://kylesethgray.com/content/images/2018/08/thanksgiving_day_challenge_5k.png",
+            {badges.map((badge, index) => (
+              <Pressable
+                key={index}
+                onPress={() => {
+                  setBadgeSelected(index);
+                  setModalVisible(true);
                 }}
-              />
-              <Text style={styles.textBadge}>Turkey Trot 2016</Text>
-            </View>
-
-            <View style={styles.badge}>
-              {/* Badge 2 */}
-              <Image
-                style={styles.imageBadge}
-                source={{
-                  uri: "https://kylesethgray.com/content/images/2018/08/National_Park_2017_01.png",
-                }}
-              />
-              <Text style={styles.textBadge}>National Parks</Text>
-            </View>
-
-            <View style={styles.badge}>
-              {/* Badge 3 */}
-              <Image
-                style={styles.imageBadge}
-                source={{
-                  uri: "https://kylesethgray.com/content/images/2018/08/20170403_MothersDay_Sticker.png",
-                }}
-              />
-              <Text style={styles.textBadge}>Earth Day 2017</Text>
-            </View>
-          </View>
-
-          <View style={styles.row}>
-            <View style={styles.badge}>
-              {/* Badge 4 */}
-              <Image
-                style={styles.imageBadge}
-                source={{
-                  uri: "https://kylesethgray.com/content/images/2018/08/earth_day_2017_01.png",
-                }}
-              />
-              <Text style={styles.textBadge}>Mothers Day 2017</Text>
-            </View>
-
-            <View style={styles.badge}>
-              {/* Badge 5 */}
-              <Image
-                style={styles.imageBadge}
-                source={{
-                  uri: "https://kylesethgray.com/content/images/2018/08/new_year_2017.png",
-                }}
-              />
-              <Text style={styles.textBadge}>National Parks</Text>
-            </View>
-
-            <View style={styles.badge}>
-              {/* Badge 6 */}
-              <Image
-                style={styles.imageBadge}
-                source={{
-                  uri: "https://kylesethgray.com/content/images/2018/08/VeteransDay_Sticker.png",
-                }}
-              />
-              <Text style={styles.textBadge}>Veterans Day</Text>
-            </View>
-          </View>
-
-          <View style={styles.row}>
-            <View style={styles.badge}>
-              {/* Badge 7 */}
-              <Image
-                style={styles.imageBadge}
-                source={{
-                  uri: "https://kylesethgray.com/content/images/2021/01/heart_month_2018_01.png",
-                }}
-              />
-              <Text style={styles.textBadge}>Heart Month 2018</Text>
-            </View>
+                style={styles.badge}
+              >
+                <Image
+                  style={styles.imageBadge}
+                  source={{
+                    uri: badge.picture,
+                  }}
+                />
+                <Text style={styles.textBadge}>{badge.name}</Text>
+              </Pressable>
+            ))}
           </View>
         </View>
+
+        <Modal visible={modalVisible} animationType="fade" transparent={true}>
+          {badgeSelected !== -1 && (
+            <View style={styles.modalView}>
+              <View style={styles.containerIcons}>
+                <Ionicons
+                  style={styles.iconClose}
+                  name="close-circle"
+                  onPress={() => setModalVisible(false)}
+                  color={"black"}
+                  size={45}
+                />
+              </View>
+
+              <View>
+                <View style={styles.containerImageModal}>
+                  <Image
+                    style={styles.imageBadgeModal}
+                    source={{ uri: badges[badgeSelected].picture }}
+                  />
+                </View>
+
+                <Text style={styles.modalTitle}>
+                  {badges[badgeSelected].name}
+                </Text>
+
+                <Text style={styles.modalRank}>
+                  {badges[badgeSelected].rank}
+                </Text>
+
+                <Text style={styles.modalDescription}>
+                  {badges[badgeSelected].description}
+                </Text>
+              </View>
+            </View>
+          )}
+        </Modal>
       </ScrollView>
     </SafeAreaView>
   );
@@ -110,10 +107,15 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     paddingHorizontal: 20,
     flex: 0.8,
+    flexWrap: "wrap",
   },
   imageBadge: {
     width: 50,
     height: 50,
+  },
+  imageBadgeModal: {
+    width: 200,
+    height: 200,
   },
   badge: {
     marginTop: 10,
@@ -125,5 +127,49 @@ const styles = StyleSheet.create({
   textBadge: {
     width: 85,
     textAlign: "center",
+  },
+  modalView: {
+    margin: 10,
+    marginTop: 0,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 10,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  containerIcons: {
+    width: "100%",
+    alignItems: "flex-end",
+    padding: 0,
+  },
+  modalTitle: {
+    textAlign: "center",
+    marginTop: 25,
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  containerImageModal: {
+    flexDirection: "column",
+    alignSelf: "center",
+  },
+  modalDescription: {
+    marginTop: 25,
+    textAlign: "center",
+    marginBottom: 25,
+  },
+  modalRank: {
+    textAlign: "center",
+    marginTop: 25,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  iconClose: {
+    fontSize: 40,
+    color: "red",
   },
 });
