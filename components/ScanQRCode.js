b44ac16c186f1512a33eb56009dd3b5c7a95f013
Matias gomez <mrmatiasgomez@gmail.com>
2023-07-20 12:09:47 +0200

    Ajout de la zone de scan avec affichage du lien

--- components/ScanQRCode.js
+++ components/ScanQRCode.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import {
   SafeAreaView,
   Alert,
@@ -16,28 +16,65 @@ const ScanQRCode = ({ route, navigation }) => {
 
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const [scanned, setScanned] = useState(false);
+  const scannerRef = useRef(null);
+  const [markerPosition, setMarkerPosition] = useState({ x: 0, y: 0 });
+  const [markerBounds, setMarkerBounds] = useState(null);
+  const [link, setLink] = useState(null);
+
+  const onLayoutChange = () => {
+    // Récupérer les dimensions du wrapper view contenant le scanner
+    const { width, height } = scannerRef.current.getBoundingClientRect();
+
+    // Mettre à jour les limites du scanner avec les dimensions du wrapper view
+    scannerRef.current.setBarCodeBounds({ x: 0, y: 0, width, height });
+  };
+
+  useEffect(() => {
+    if (!permission?.granted) requestPermission();
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (scanned) {
+      // Scanner à nouveau après 0,5 seconde
+      const interval = setInterval(() => {
+        setScanned(false);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [scanned]);
 
   if (!permission?.granted) requestPermission();
 
-  const handleBarCodeScanned = (data) => {
+  const handleBarCodeScanned = ({ type, data, bounds }) => {
     if (route.name !== "Scan" || !navigation.isFocused()) return;
     setScanned(true);
     console.log(data);
+    setLink(data);
 
-    Alert.alert("Nouveau client", " ", [
-      {
-        text: "Refuser",
-        onPress: () => {
-          setScanned(false);
-        },
-      },
-      {
-        text: "Accepter",
-        onPress: () => {
-          setScanned(false);
-        },
-      },
-    ]);
+    const { origin, size } = bounds;
+    const x = origin.x;
+    const y = origin.y;
+
+    setMarkerPosition({ x, y });
+    setMarkerBounds(bounds);
+
+    // Alert.alert("Nouveau client", " ", [
+    //   {
+    //     text: "Refuser",
+    //     onPress: () => {
+    //       setScanned(false);
+    //     },
+    //   },
+    //   {
+    //     text: "Accepter",
+    //     onPress: () => {
+    //       setScanned(false);
+    //     },
+    //   },
+    // ]);
   };
 
   return (
@@ -46,6 +83,26 @@ const ScanQRCode = ({ route, navigation }) => {
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={{ flex: 1, width: "100%" }}
       />
+
+      {scanned && markerBounds && (
+        <View
+          style={[
+            styles.marker,
+            {
+              left: markerBounds.origin.x,
+              top: markerBounds.origin.y,
+              width: markerBounds.size.width,
+              height: markerBounds.size.height,
+            },
+          ]}
+        >
+          {scanned && markerBounds && (
+            <Pressable style={styles.linkMessage}>
+              <Text style={styles.linkText}>{link}</Text>
+            </Pressable>
+          )}
+        </View>
+      )}
     </View>
   );
 };
@@ -86,6 +143,39 @@ const styles = StyleSheet.create({
   modalText: {
     marginBottom: 15,
     textAlign: "center",
+  },
+
+  scannerContainer: {
+    position: "relative",
+    overflow: "hidden",
+  },
+  marker: {
+    position: "absolute",
+    borderWidth: 2,
+    borderColor: "green",
+    borderRadius: 10,
+    width: 200, // Largeur du marqueur
+    height: 100, // Hauteur du marqueur
+  },
+  linkMessage: {
+    position: "relative",
+    top: "105%",
+    left: 0,
+    right: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    overflow: "hidden",
+    width: "100%",
+  },
+  linkText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "black",
+    backgroundColor: "white",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    width: "100%",
   },
 });
 
